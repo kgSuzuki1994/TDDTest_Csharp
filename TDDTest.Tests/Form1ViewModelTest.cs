6777cd549e8e89bcdf009b99cfd5b84c8bfac3d7
kgSuzuki1994 <takuti0511@gmail.com>
2020-05-06 22:27:55 +0900

    ViewModelのテスト作成

---
+++ TDDTest.Tests/Form1ViewModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD.UI;
+
+namespace TDDTest.Tests
+{
+    [TestClass]
+    public class Form1ViewModelTest
+    {
+        [TestMethod]
+        public void シナリオ()
+        {
+            var viewModel = new Form1ViewModel();
+            Assert.AreEqual("", viewModel.ATextBoxText);
+            Assert.AreEqual("", viewModel.BTextBoxText);
+            Assert.AreEqual("", viewModel.ResultLabelText);
+        }
+    }
+}
